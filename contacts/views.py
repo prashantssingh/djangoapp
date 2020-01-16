128ab05f74595e911fa02b4b33e910b7f068ec41
Prashant S. Singh <prashant_singh@live.in>
2020-01-15 22:28:55 -0600

    Contact: update views

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. Contact app using Django")
