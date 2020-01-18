cdef4e619af02ca38818598fd8660122041a91bb
Prashant Singh <prashant_singh@live.in>
2020-01-17 23:04:49 -0600

    create model for contact app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.IntegerField(default=0)
+    email = models.CharField(max_length=200)
