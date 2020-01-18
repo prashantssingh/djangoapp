ee053ab259927761f854dd69a16326bc6aa208e8
Prashant Singh <prashant_singh@live.in>
2020-01-17 23:08:40 -0600

    add new view in contact

--- contacts/views.py
+++ contacts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, contact_id):
+    return HttpResponse("You're looking at contact %s." % contact_id)
