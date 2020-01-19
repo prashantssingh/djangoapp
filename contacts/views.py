27fd52e430e7312bdca477e34d463298f0dc4fac
Prashant Singh <prashant_singh@live.in>
2020-01-18 23:54:54 -0600

    update index view

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. Contact app using Django")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, contact_id):
     return HttpResponse("You're looking at contact %s." % contact_id)
