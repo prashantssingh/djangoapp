8811f4399ae297596f7cef0e95f5e338e7f3a39b
Prashant Singh <prashant_singh@live.in>
2020-01-18 23:54:12 -0600

    contact: add detail url

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('/contactform', views.contactform, name='contact_form'),
+    path('/detail', views.detail, name='contact_detail'),
 ]
