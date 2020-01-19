641d64c307c32bbec4c883f74eb46433028e7b17
Prashant Singh <prashant_singh@live.in>
2020-01-18 23:59:47 -0600

    bugfixes in contact views

--- contacts/views.py
+++ contacts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Contact.objects.order_by('-pub_date')[:5]
     context = {'last_contact': latest_question_list}
     return render(request, 'contact/index.html', context)
 
 def detail(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
+        question = Contact.objects.get(pk=question_id)
+    except Contact.DoesNotExist:
         raise Http404("Contact does not exist")
     return render(request, 'contact/detail.html', {'question': question})
